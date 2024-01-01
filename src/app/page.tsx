0530ab7c0b816ece3fd0e68c7056a3eb8d9eb930
Ayush0054 <ayush5aprill@gmail.com>
2024-01-02 03:31:39 +0530

    drawer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { DrawerDemo } from "./component/first";
 
 export default function Home() {
   return (
     <div>
       <Button>Click me</Button>
+      <DrawerDemo />
     </div>
   );
 }
