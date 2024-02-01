47b04b93405921ef1451e63928b404000c9be288
Ayush0054 <ayush5aprill@gmail.com>
2024-02-02 02:47:18 +0530

    skeleton for avtar :sparkles:

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -1,10 +1,26 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import { UserButton } from "@clerk/nextjs";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Navbar() {
+  const [userButtonReady, setUserButtonReady] = useState(false);
+
+  useEffect(() => {
+    // Set a timeout to simulate the loading of the UserButton component
+    const timeoutId = setTimeout(() => {
+      setUserButtonReady(true);
+    }, 2000); // Set the timeout to 2000ms (2 seconds) for demonstration purposes
+
+    // Clean up the timeout when the component is unmounted
+    return () => clearTimeout(timeoutId);
+  }, []);
   return (
     <div className="flex justify-end  p-4 border-b-2  ">
-      <UserButton afterSignOutUrl="/" />
+      {!userButtonReady ? (
+        <Skeleton className="h-10 w-10 rounded-full" />
+      ) : (
+        <UserButton afterSignOutUrl="/" />
+      )}
     </div>
   );
 }
